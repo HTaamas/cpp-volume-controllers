4239a97e9f770ef2ed8b9dae37fdff4aa698ee01
HTaamas <huntamas2222@gmail.com>
2026-06-30 16:00:28 +0200

    made skip and previous track require double tap of the keybind

--- src/volume_handler_win.cpp
+++ src/volume_handler_win.cpp
@@ -1,5 +1,6 @@
 #include "volume_handler.h"
 #include <QDebug>
+#include <QElapsedTimer>
 
 #ifdef _WIN32
 HHOOK VolumeHandler::hHook = nullptr;
@@ -27,6 +28,12 @@ VolumeHandler::~VolumeHandler() {
 }
 
 LRESULT CALLBACK VolumeHandler::LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    const bool isShift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
+    const bool isCtrl = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
+    static QElapsedTimer nextTrackTimer;
+    static QElapsedTimer prevTrackTimer;
+    const int doubleTapTimeout = 400;
+
     if (nCode == HC_ACTION) {
         KBDLLHOOKSTRUCT *pKey = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam);
         if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
@@ -45,15 +52,23 @@ LRESULT CALLBACK VolumeHandler::LowLevelKeyboardProc(int nCode, WPARAM wParam, L
             }
 
             if (pKey->vkCode == instance->keybindSettings.mainKey.toInt(nullptr, 16)) {
-                const bool isShift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
-                const bool isCtrl = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
                 if (instance) {
                     if (isShift && isCtrl) {
                         return 0;
                     } else if (isShift) {
-                        emit instance->nextTrack();
+                        if (!nextTrackTimer.isValid() || nextTrackTimer.elapsed() > doubleTapTimeout) {
+                            nextTrackTimer.start();
+                        } else {
+                            nextTrackTimer.invalidate();
+                            emit instance->nextTrack();
+                        }
                     } else if (isCtrl) {
-                        emit instance->prevTrack();
+                        if (!prevTrackTimer.isValid() || prevTrackTimer.elapsed() > doubleTapTimeout) {
+                            prevTrackTimer.start();
+                        } else {
+                            prevTrackTimer.invalidate();
+                            emit instance->prevTrack();
+                        }
                     } else {
                         emit instance->toggleMusic();
                     }
@@ -63,7 +78,6 @@ LRESULT CALLBACK VolumeHandler::LowLevelKeyboardProc(int nCode, WPARAM wParam, L
             }
 
             if (pKey->vkCode == VK_VOLUME_UP || pKey->vkCode == VK_VOLUME_DOWN) {
-                const bool isShift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
                 const bool useFineStep = instance && instance->keybindSettings.useShiftForFineAdjust && isShift;
                 const int step = instance ? (useFineStep ? instance->keybindSettings.fineStep : instance->keybindSettings.coarseStep) : 5;
                 const int delta = (pKey->vkCode == VK_VOLUME_UP) ? step : -step;
